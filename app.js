36153ff2cdd745cb83f5396ac486478492a1492e
Connor Harvey <harvey.connor@rocketmail.com>
2023-03-19 12:57:26 +0000

    first commit

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+const https = require("https");
+
+app.use(express.static("public"));
+app.use(express.urlencoded());
+
+app.get("/", function(req, res) {
+    res.sendFile(__dirname + "/home.html");
+});
+
+
+
+app.listen("3000", function() {
+    console.log("server is running on port 3000");
+});
