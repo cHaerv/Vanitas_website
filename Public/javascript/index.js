bd22bd85cad889516ac7edd60bdd40d3e74c2911
Connor H <125142126+cHaerv@users.noreply.github.com>
2023-05-23 12:16:27 +0100

    finish music page and add javascript features

--- Public/javascript/index.js
+++ Public/javascript/index.js
@@ -1,2 +1,13 @@
+let overseer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add("reveal");
+        }
+    });
+});
 
+
+let hideScrollElements = document.querySelectorAll(".hide");
+
+hideScrollElements.forEach((el) => overseer.observe(el));
 
